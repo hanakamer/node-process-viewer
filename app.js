b3b5caeaca1d567f013eb7426feb540cbdfc4c37
hana can <hanacan@hanas-MacBook-Pro.local>
2016-10-06 16:31:53 +0300

    https

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var http  = require('http')
+var https  = require('https')
 var fs = require('fs');
 var shellParser = require('node-shell-parser');
 var child = require('child_process');
@@ -42,7 +42,7 @@ function getData(callback){
   }
 
 }
-var server = http.createServer(function(req, res){
+var server = https.createServer(function(req, res){
   fs.readFile('./index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
